212b34f2561d1ed4998619e4e9aeabd5e9135b88
Shashank <shashank906@gmail.com>
2021-12-03 03:53:15 -0600

    day 1 part 2

--- src/day1/sol.py
+++ src/day1/sol.py
@@ -16,7 +16,21 @@ def part1(data):
 
 
 def part2(data):
-    return ""
+    increases = 0
+    last_window = None
+
+    for i, val in enumerate(data):
+        if last_window is None and len(data) > i + 2:
+            last_window = data[i] + data[i + 1] + data[i + 2]
+            continue
+        if len(data) <= i + 2:
+            break
+
+        curr_window = data[i] + data[i + 1] + data[i + 2]
+        if curr_window > last_window:
+            increases += 1
+        last_window = curr_window
+    return increases
 
 
 data = read_data(sys.argv[1], int)
