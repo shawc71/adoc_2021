6377a9daacfc71a80407e16afd458cdd961b6810
Shashank <shashank906@gmail.com>
2021-12-12 05:37:57 -0600

    day 7 part 2

--- src/day7/sol.py
+++ src/day7/sol.py
@@ -1,6 +1,7 @@
 import sys
 from ..common import read_data
 
+
 def parse(data):
     return sorted(list(map(lambda val: int(val), data.split(","))))
 
@@ -12,12 +13,20 @@ def calc_fuel(alignment_candidate, positions):
     return total_fuel
 
 
-def part1(data):
+def calc_fuel_part_2(alignment_candidate, positions):
+    total_fuel = 0
+    for position in positions:
+        no_of_moves = abs(alignment_candidate - position)
+        total_fuel += (no_of_moves * (no_of_moves + 1)) / 2
+    return total_fuel
+
+
+def solve(data, fuel_calc_func):
     positions = parse(data)
     alignment_candidate = positions[0]
     total_fuel = {}
     while alignment_candidate <= positions[-1]:
-        total_fuel[alignment_candidate] = calc_fuel(alignment_candidate, positions)
+        total_fuel[alignment_candidate] = fuel_calc_func(alignment_candidate, positions)
         alignment_candidate += 1
 
     lowest = None
@@ -27,8 +36,12 @@ def part1(data):
     return lowest
 
 
+def part1(data):
+    return solve(data, calc_fuel)
+
+
 def part2(data):
-    return ""
+    return solve(data, calc_fuel_part_2)
 
 
 data = read_data(sys.argv[1], str)[0]
