ae6371db25ed09978d23b675e39aace22510603e
Shashank <shashank906@gmail.com>
2021-12-12 22:37:16 -0600

    day 8 part 2

--- src/day8/sol.py
+++ src/day8/sol.py
@@ -2,22 +2,29 @@
 import sys
 from ..common import read_data
 
-segment_counts = {
-    1: 2,
+mapping_by_segment_count = {
+    2: 1,
     4: 4,
-    7: 3,
-    8: 7,
+    3: 7,
+    7: 8,
 }
 unique_segment_counts = {2, 4, 3, 7}
+
+
 def parse(data):
     input = []
     for line in data:
         patterns, output = line.split(" | ")
+        patterns = list(map(lambda word: "".join(sorted(word)), patterns.split()))
+        output = list(map(lambda word: "".join(sorted(word)), output.split()))
+
         input.append({
-            'signal_patterns': patterns.split(),
-            'output': output.split(),
+            'signal_patterns': patterns,
+            'output': output,
         })
+
     return input
+
 
 def part1(data):
     inputs = parse(data)
@@ -30,11 +37,49 @@ def part1(data):
     return total
 
 
+def difference(first, second):
+    return len(set(first).difference(set(second)))
+
+
+def intersection(first, second):
+    return len(set(first).intersection(set(second)))
+
+
 def part2(data):
-    return ""
+    encodings = {}
+    inputs = parse(data)
+    totals = 0
+    for input in inputs:
+        for pattern in input['signal_patterns']:
+            if len(pattern) in mapping_by_segment_count:
+                encodings[mapping_by_segment_count[len(pattern)]] = pattern
+
+        for pattern in input['signal_patterns']:
+            if len(pattern) == 5:
+                if difference(pattern, encodings[1]) == 3:
+                    encodings[3] = pattern
+                elif difference(pattern, encodings[4]) == 3:
+                    encodings[2] = pattern
+                else:
+                    encodings[5] = pattern
+            elif len(pattern) == 6:
+                if intersection(encodings[4], pattern) == 4:
+                    encodings[9] = pattern
+                elif intersection(encodings[1], pattern) == 1:
+                    encodings[6] = pattern
+                else:
+                    encodings[0] = pattern
+        pattern_to_encoding_map = {}
+        for k, v in encodings.items():
+            pattern_to_encoding_map[v] = k
+
+        num = ""
+        for output in input['output']:
+            num += str(pattern_to_encoding_map[output])
+        totals += int(num)
+    return totals
 
 
 data = read_data(sys.argv[1], str)
 print(f"Part 1: {part1(data)}")
 print(f"Part 2: {part2(data)}")
-
