c46d07ccb136834c3070ef74a8d21d7ad48d35ee
Shashank <shashank906@gmail.com>
2021-12-24 20:40:45 -0600

    day 10 part 2

--- src/day10/sol.py
+++ src/day10/sol.py
@@ -1,4 +1,5 @@
 # https://adventofcode.com/2021/day/10
+import math
 import sys
 from ..common import read_data
 from collections import deque
@@ -17,11 +18,20 @@ corruption_points = {
     '>': 25137,
 }
 
+autocomplete_points = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4,
+}
+
+
 def parse(data):
     inputs = []
     for line in data:
         inputs.append(list(line))
     return inputs
+
 
 def corrupted_at(tokens):
     stack = deque()
@@ -34,6 +44,25 @@ def corrupted_at(tokens):
             return token
     return None
 
+
+def autocomplete(tokens):
+    stack = deque()
+    for token in tokens:
+        if token in open_chars:
+            stack.append(token)
+            continue
+        top = stack.pop()
+        assert open_close_mappings[top] == token
+    if not stack:
+        return []
+    ret = []
+    while len(stack) != 0:
+        top = stack.pop()
+        closing = open_close_mappings[top]
+        ret.append(closing)
+    return ret
+
+
 def part1(data):
     inputs = parse(data)
     score = 0
@@ -44,13 +73,22 @@ def part1(data):
     return score
 
 
-
 def part2(data):
-    return ""
+    inputs = parse(data)
+    scores = []
+    for i in inputs:
+        corrupted_token = corrupted_at(i)
+        if corrupted_token is not None:
+            continue
+        score = 0
+        for completion_token in autocomplete(i):
+            score = (score * 5) + autocomplete_points[completion_token]
+        scores.append(score)
+    scores = sorted(scores)
+    middle_idx = math.floor(len(scores)/2)
+    return scores[middle_idx]
 
 
 data = read_data(sys.argv[1], str)
-print("-----------------------------------------------------------")
 print(f"Part 1: {part1(data)}")
 print(f"Part 2: {part2(data)}")
-
