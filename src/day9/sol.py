66401e69573ea1de01b029a943e6410ca6d4f437
Shashank <shashank906@gmail.com>
2021-12-24 19:55:56 -0600

    day 9 part 2

--- src/day9/sol.py
+++ src/day9/sol.py
@@ -23,13 +23,13 @@ def get_adjacent(i, j, area):
     final = []
     for c in candidates:
         if min_i <= c[0] < max_i and min_j <= c[1] < max_j:
-            final.append(area[c[0]][c[1]])
+            final.append(c)
     return final
 
 
-def is_low_point(target_point, adjacent_pts):
-    for adj_point in adjacent_pts:
-        if target_point >= adj_point:
+def is_low_point(target_point, adjacent_pts, area):
+    for point in adjacent_pts:
+        if target_point >= area[point[0]][point[1]]:
             return False
     return True
 
@@ -40,13 +40,43 @@ def part1(data):
     for i, _ in enumerate(area):
         for j, _ in enumerate(area[i]):
             adjacent_pts = get_adjacent(i, j, area)
-            if is_low_point(area[i][j], adjacent_pts):
+            if is_low_point(area[i][j], adjacent_pts, area):
                 answer += area[i][j] + 1
     return answer
 
 
+def calc_basin_size(curr_point, visited, area):
+    i, j = curr_point
+    if area[i][j] == 9:
+        return 0
+
+    adjacent_pts = list(filter(lambda pt: pt not in visited and area[pt[0]][pt[1]] != 9, get_adjacent(i, j, area)))
+    # print(adjacent_pts)
+    if not is_low_point(area[i][j], adjacent_pts, area):
+        return 0
+
+    visited.add((i, j))
+    basin_size = 1
+    for a in adjacent_pts:
+        basin_size += calc_basin_size(a, visited, area)
+    return basin_size
+
+
+def solve_part2(data):
+    area = parse(data)
+    visited = set()
+    basin_sizes = []
+    for i, _ in enumerate(area):
+        for j, _ in enumerate(area[i]):
+            basin_size = calc_basin_size((i, j), visited, area)
+            if basin_size > 0:
+                basin_sizes.append(basin_size)
+    # basin_sizes = sorted(basin_sizes)
+    return basin_sizes
+
+
 def part2(data):
-    return ""
+    return solve_part2(data)
 
 
 data = read_data(sys.argv[1], str)
