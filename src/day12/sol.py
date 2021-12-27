73ee295316d66d78ea69aef9cbc3280578180697
Shashank <shashank906@gmail.com>
2021-12-27 05:52:58 -0600

    day 12 part 2

--- src/day12/sol.py
+++ src/day12/sol.py
@@ -1,6 +1,5 @@
 # https://adventofcode.com/2021/day/12
 import sys
-import copy
 from ..common import read_data
 
 
@@ -28,6 +27,7 @@ def parse(lines):
         graph[path[1]].add(path[0])
     return graph
 
+
 def paths_to_end(start_cave, graph, visited_small_caves):
     to_end = 0
     for next_cave in graph[start_cave]:
@@ -45,6 +45,7 @@ def paths_to_end(start_cave, graph, visited_small_caves):
                 visited_small_caves.remove(next_cave)
     return to_end
 
+
 def part1(data):
     graph = parse(data)
     path_count = 0
@@ -56,30 +57,35 @@ def part1(data):
     return path_count
 
 
-def paths_to_end_2(start_cave, graph, visited_small_caves, s):
+def paths_to_end_2(start_cave, graph, visited_small_caves, can_visit_twice, path, valid_paths):
+    global accum
     to_end = 0
     for next_cave in graph[start_cave]:
         if next_cave == 'start':
             continue
-        if (next_cave == s and s in visited_small_caves and visited_small_caves[s] > 1) or \
-                (next_cave != s and (next_cave in visited_small_caves and visited_small_caves[next_cave] == 1)):
+        if (next_cave == can_visit_twice and can_visit_twice in visited_small_caves and visited_small_caves[
+            can_visit_twice] > 1) or \
+                (next_cave != can_visit_twice and (
+                        next_cave in visited_small_caves and visited_small_caves[next_cave] == 1)):
             continue
         if next_cave == 'end':
-            print(start_cave)
+            valid_paths.add("".join(path))
             to_end += 1
         else:
             if is_small(next_cave):
                 if next_cave not in visited_small_caves:
                     visited_small_caves[next_cave] = 0
                 visited_small_caves[next_cave] += 1
-            # print(f"{start_cave}-{next_cave}")
-            to_end += paths_to_end_2(next_cave, graph, copy.deepcopy(visited_small_caves), s)
+            path.append(next_cave)
+            to_end += paths_to_end_2(next_cave, graph, visited_small_caves, can_visit_twice, path, valid_paths)
+            path.pop()
             if is_small(next_cave):
                 if visited_small_caves[next_cave] == 0:
                     visited_small_caves[next_cave] = 0
                 else:
                     visited_small_caves[next_cave] -= 1
     return to_end
+
 
 def part2(data):
     graph = parse(data)
@@ -88,16 +94,16 @@ def part2(data):
     for key in graph:
         if is_small(key) and key != 'start' and key != 'end':
             small_caves.append(key)
-    print(small_caves)
+    valid_paths = set()
     for cave in graph['start']:
-        # if cave != 'b':
-        #     continue
-        for s in small_caves:
+        for can_visit_twice in small_caves:
             visited_small_caves = {}
+            path = [cave]
             if is_small(cave):
                 visited_small_caves[cave] = 1
-            path_count += paths_to_end_2(cave, graph, visited_small_caves, s)
-    return path_count
+            path_count += paths_to_end_2(cave, graph, visited_small_caves, can_visit_twice, path, valid_paths)
+    return len(valid_paths)
+
 
 data = read_data(sys.argv[1], str)
 print(f"Part 1: {part1(data)}")
