7376cbcff1f24cefb404bea9905930931f50d221
Shashank <shashank906@gmail.com>
2021-12-27 05:26:33 -0600

    saving

--- src/day12/sol.py
+++ src/day12/sol.py
@@ -1,5 +1,6 @@
 # https://adventofcode.com/2021/day/12
 import sys
+import copy
 from ..common import read_data
 
 
@@ -55,9 +56,48 @@ def part1(data):
     return path_count
 
 
-def part2(data):
-    return ""
+def paths_to_end_2(start_cave, graph, visited_small_caves, s):
+    to_end = 0
+    for next_cave in graph[start_cave]:
+        if next_cave == 'start':
+            continue
+        if (next_cave == s and s in visited_small_caves and visited_small_caves[s] > 1) or \
+                (next_cave != s and (next_cave in visited_small_caves and visited_small_caves[next_cave] == 1)):
+            continue
+        if next_cave == 'end':
+            print(start_cave)
+            to_end += 1
+        else:
+            if is_small(next_cave):
+                if next_cave not in visited_small_caves:
+                    visited_small_caves[next_cave] = 0
+                visited_small_caves[next_cave] += 1
+            # print(f"{start_cave}-{next_cave}")
+            to_end += paths_to_end_2(next_cave, graph, copy.deepcopy(visited_small_caves), s)
+            if is_small(next_cave):
+                if visited_small_caves[next_cave] == 0:
+                    visited_small_caves[next_cave] = 0
+                else:
+                    visited_small_caves[next_cave] -= 1
+    return to_end
 
+def part2(data):
+    graph = parse(data)
+    path_count = 0
+    small_caves = []
+    for key in graph:
+        if is_small(key) and key != 'start' and key != 'end':
+            small_caves.append(key)
+    print(small_caves)
+    for cave in graph['start']:
+        # if cave != 'b':
+        #     continue
+        for s in small_caves:
+            visited_small_caves = {}
+            if is_small(cave):
+                visited_small_caves[cave] = 1
+            path_count += paths_to_end_2(cave, graph, visited_small_caves, s)
+    return path_count
 
 data = read_data(sys.argv[1], str)
 print(f"Part 1: {part1(data)}")
