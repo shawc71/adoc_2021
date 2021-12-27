39379c1e14178fa15321a201dccc995442558505
Shashank <shashank906@gmail.com>
2021-12-27 07:58:38 -0600

    added a helper

--- src/common.py
+++ src/common.py
@@ -2,6 +2,7 @@ def read_data(data_file, evaluator=str):
     with open(data_file) as f:
         return [evaluator(v) for v in f.read().rstrip().split("\n")]
 
+
 def print_2d_array(grid, chars_per_slot):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
