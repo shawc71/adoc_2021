e28166200654e175490cc13432fc4faff8627927
Shashank <shashank906@gmail.com>
2021-12-30 19:30:13 -0600

    cleaned up

--- src/day15/sol.py
+++ src/day15/sol.py
@@ -5,6 +5,15 @@ import sys
 from ..common import *
 
 
+class PqItem:
+    def __init__(self, priority, data):
+        self.priority = priority
+        self.data = data
+
+    def __lt__(self, other):
+        return self.priority < other.priority
+
+
 def parse(data):
     grid = []
     for line in data:
@@ -20,23 +29,20 @@ def part1(data):
     risks_from_origin = [[math.inf for _ in range(len(grid))] for _ in range(len(grid[0]))]
     risks_from_origin[0][0] = 0
     pq = []
+    pq_lookup = {}
     for i, _ in enumerate(risks_from_origin):
         for j, _ in enumerate(risks_from_origin[i]):
-            item = (risks_from_origin[i][j], (i, j))
+            item = PqItem(risks_from_origin[i][j], (i, j))
+            pq_lookup[item.data] = item
             heapq.heappush(pq, item)
 
     while len(pq) != 0:
         node = heapq.heappop(pq)
-        for adj in get_adjacent_points(grid, node[1], False):
-            if node[0] + grid[adj[0]][adj[1]] < risks_from_origin[adj[0]][adj[1]]:
-                risks_from_origin[adj[0]][adj[1]] = node[0] + grid[adj[0]][adj[1]]
-                idx = None
-                for i, _ in enumerate(pq):
-                    if pq[i][1] == (adj[0], adj[1]):
-                        idx = i
-                        break
-                pq.pop(idx)
-                pq.append((risks_from_origin[adj[0]][adj[1]], (adj[0], adj[1])))
+        for adj in get_adjacent_points(grid, node.data, False):
+            candidate_risk = node.priority + grid[adj[0]][adj[1]]
+            if candidate_risk < risks_from_origin[adj[0]][adj[1]]:
+                risks_from_origin[adj[0]][adj[1]] = candidate_risk
+                pq_lookup[adj].priority = candidate_risk
                 heapq.heapify(pq)
     return risks_from_origin[len(risks_from_origin) - 1][len(risks_from_origin[0]) - 1]
 
