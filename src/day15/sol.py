c7e2a3b83fe75bbdbf1d454f74baa0ec5f80942c
Shashank <shashank906@gmail.com>
2021-12-30 23:24:55 -0600

    day 15 part 2

--- src/day15/sol.py
+++ src/day15/sol.py
@@ -24,18 +24,15 @@ def parse(data):
     return grid
 
 
-def part1(data):
-    grid = parse(data)
+def find_lowest_risk_path(grid):
     risks_from_origin = [[math.inf for _ in range(len(grid))] for _ in range(len(grid[0]))]
     risks_from_origin[0][0] = 0
     pq = []
     pq_lookup = {}
-
     item = PqItem(risks_from_origin[0][0], (0, 0))
     pq_lookup[item.data] = item
     heapq.heappush(pq, item)
     visited = set()
-
     while len(pq) != 0:
         node = heapq.heappop(pq)
         if node.data in visited:
@@ -55,12 +52,32 @@ def part1(data):
                     item = PqItem(candidate_risk, adj)
                     pq_lookup[item.data] = item
                     heapq.heappush(pq, item)
-
     return risks_from_origin[len(risks_from_origin) - 1][len(risks_from_origin[0]) - 1]
 
 
+def reformat_for_part2(grid):
+    new_grid = [[0 for _ in range(len(grid) * 5)] for _ in range(len(grid[0]) * 5)]
+    grid_len = len(grid)
+    grid_width = len(grid[0])
+
+    for i, _ in enumerate(new_grid):
+        factor = math.floor(i / grid_len)
+        for j, _ in enumerate(new_grid[i]):
+            new_grid[i][j] = (grid[i % grid_len][j % grid_width] + factor)
+            if new_grid[i][j] > 9:
+                new_grid[i][j] = new_grid[i][j] % 9
+            if (j + 1) % grid_width == 0:
+                factor += 1
+    return new_grid
+
+
+def part1(data):
+    return find_lowest_risk_path(parse(data))
+
+
 def part2(data):
-    return ""
+    grid = parse(data)
+    return find_lowest_risk_path(reformat_for_part2(grid))
 
 
 data = read_data(sys.argv[1], str)
