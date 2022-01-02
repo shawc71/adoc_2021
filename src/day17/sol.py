a895e5fcdb613aac898e2f43880aa94f7a3e7b34
Shashank <shashank906@gmail.com>
2022-01-02 04:27:02 -0600

    day 17 part 2

--- src/day17/sol.py
+++ src/day17/sol.py
@@ -8,6 +8,7 @@ def parse_area(area_str):
     start, end = area_str.split("=", 2)[1].split("..")
     return int(start), int(end)
 
+
 def parse(data):
     x_area, y_area = data.split(": ", 2)[1].split(", ")
     x = parse_area(x_area)
@@ -16,17 +17,11 @@ def parse(data):
 
 
 def launch(x_target, y_target, x_velocity, y_velocity):
-    debug = False
     max_y = 0
     x, y = 0, 0
     while x < x_target[1] and y >= y_target[0]:
-        if debug:
-            print("-------------")
-            print(f"x-vel:{x_velocity}, y-vel:{y_velocity}")
         x += x_velocity
         y += y_velocity
-        if debug:
-            print(f"x:{x}, y:{y}")
         if y > max_y:
             max_y = y
         if x_target[0] <= x <= x_target[1] and y_target[0] <= y <= y_target[1]:
@@ -38,12 +33,12 @@ def launch(x_target, y_target, x_velocity, y_velocity):
         y_velocity -= 1
     return False, None
 
+
 def part1(data):
     x, y = parse(data)
     candidates = []
-    for i in range(x[1]):
-        for j in range(math.floor(x[1]/2)):
-            # print(i, j)
+    for i in range(x[1] + 1):
+        for j in range(math.floor(x[1] / 2)):
             hit, max_height = launch(x, y, i, j)
             if hit:
                 candidates.append(max_height)
@@ -51,10 +46,16 @@ def part1(data):
 
 
 def part2(data):
-    return ""
+    x, y = parse(data)
+    total = 0
+    for i in range(x[1] + 1):
+        for j in range(y[0], x[1] + 1):
+            hit, _ = launch(x, y, i, j)
+            if hit:
+                total += 1
+    return total
 
 
 data = "".join(read_data(sys.argv[1], str))
 print(f"Part 1: {part1(data)}")
 print(f"Part 2: {part2(data)}")
-
